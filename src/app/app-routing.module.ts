356dfb82d86396c9ef53013159d3215bf4458ebb
alezzard <gabriel.alegodoy92@gmail.com>
2022-09-21 11:13:16 -0300

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaAdministradorComponent } from './components/vista-administrador/vista-administrador.component';
+import { VistaEmpleadoComponent } from './components/vista-empleado/vista-empleado.component';
+import { VistaPrincipalComponent } from './components/vista-principal/vista-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:VistaPrincipalComponent},
+  {path:"empleado",component:VistaEmpleadoComponent},
+  {path:"administrador",component:VistaAdministradorComponent},
+  {path:"",redirectTo:"home",pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
