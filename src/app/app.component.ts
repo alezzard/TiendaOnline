936d396af7899a64caa6e78cd9751f97645596d6
alezzard <gabriel.alegodoy92@gmail.com>
2022-09-22 12:44:51 -0300

    borrando repetidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TiendaOnline';
+
+  showMessage: boolean = true;
+
+  onClickShowMessage(){
+    console.log("showMessage: ",this.showMessage)
+    this.showMessage = !this.showMessage;
+  }
+
 }
